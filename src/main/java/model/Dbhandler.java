27496e3d4ae139bba21680feb7fdd9f157ab34e0
Ishanadhik <adkishan123@gmail.com>
2023-03-30 13:17:34 +0545

    complete work

--- src/main/java/model/Dbhandler.java
+++ src/main/java/model/Dbhandler.java
@@ -36,7 +36,7 @@ public class Dbhandler {
         return connection;
     }
 
-    public static List<model.user> getAllUsers(Connection connection) {
+    public static List<user> getAllUsers(Connection connection) {
         String sql = "SELECT * FROM " + TABLE_USER;
 
         List<user> usersList = new ArrayList<>();
@@ -47,7 +47,8 @@ public class Dbhandler {
                 int id = rs.getInt(COLUMN_ID);
                 String username = rs.getString(COLUMN_USERNAME);
                 String password = rs.getString(COLUMN_PASSWORD);
-
+                user user_info= new user(id,username,password);
+                usersList.add(user_info);
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -55,7 +56,8 @@ public class Dbhandler {
         return usersList;
     }
 
-    public static List<model.Class> getAllClass(Connection connection) {
+    public static List<Class> getAllClasses(Connection connection) {
+
         String sql = "SELECT * FROM " + TABLE_CLASS;
 
         List<Class> classList = new ArrayList<>();
@@ -64,26 +66,63 @@ public class Dbhandler {
             ResultSet rs = stmt.executeQuery(sql);
             while (rs.next()) {
                 int id = rs.getInt(COLUMN_ID);
-                String username = rs.getString(COLUMN_CLASSNAME);
+                String classname = rs.getString(COLUMN_CLASSNAME);
+
+                Class class_entry= new Class(id,classname);
+                classList.add(class_entry);
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
         return classList;
     }
+    public static void add_class(Connection conn, Class c) {
 
-    public static void addUsers(Connection connection) {
-        String classname = Request(Class.);
-        String sql = "INSERT INTO " + TABLE_CLASS + " (" + COLUMN_ID + " " + COLUMN_CLASSNAME + " )" + " VALUES(?, ?)";
-        try (PreparedStatement ps = connection.prepareStatement(sql)) {
-            ps.setString(1, COLUMN_CLASSNAME);
-            ps.executeQuery();
-            connection.close();
+        String sql = "INSERT INTO " + TABLE_CLASS + " (classname)" + "VALUES (?)";
+        PreparedStatement statement = null;
+        try {
+            statement = conn.prepareStatement(sql);
+            statement.setString(1, c.getClassname());
+            statement.execute();
+            statement.close();
 
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            System.out.println(e.getMessage());
+        }
+    }
+    public static void add_attendance(Connection conn,user u ,Class c){
 
+        String sql = "INSERT INTO " + TABLE_ATTENDANCE + " (user_id,class_id)" + "VALUES (?,?)";
+        PreparedStatement statement = null;
+        try {
+            statement = conn.prepareStatement(sql);
+            statement.setInt(1, u.getId());
+            statement.setInt(2, c.getId());
+            statement.execute();
+            statement.close();
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void addUser(Connection conn, user u) {
+
+        String sql = "INSERT INTO " + TABLE_USER + " (username,password)" + "VALUES (?,?)";
+        PreparedStatement statement = null;
+        try {
+            statement = conn.prepareStatement(sql);
+            statement.setString(1, u.getUsername());
+            statement.setString(2, u.getPassword());
+            statement.execute();
+            statement.close();
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
         }
 
+
     }
-}
+
+    }
+
--- src/main/java/model/main.java
+++ src/main/java/model/main.java
@@ -1,13 +1,77 @@
 package model;
 
+import java.sql.Connection;
+import java.util.List;
 import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-
-        Dbhandler.connect();
+        Connection connection = Dbhandler.connect();
         Scanner scanner1=new Scanner(System.in);
 
-        System.out.println("enter name");
+        List<user> user_list = Dbhandler.getAllUsers(Dbhandler.connect());
+        List<Class> class_list = Dbhandler.getAllClasses(Dbhandler.connect());
+        boolean end=true;
+        while(end){
+            System.out.println("Choose number to do a particular task");
+            System.out.println("1-add User");
+            System.out.println("2-add Class");
+            System.out.println("3-add Attendance");
+            System.out.println("4-Display Users");
+            System.out.println("5-Display Classes");
+            System.out.println("6-close");
+            int select = scanner1.nextInt();
+
+            switch(select){
+                case 1:
+                    System.out.println("enter username");
+                    String username = scanner1.next();
+                    System.out.println("enter password");
+                    String password = scanner1.next();
+                    user user = new user(0,username,password);
+                    Dbhandler.addUser(connection,user);
+                    break;
+
+                case 2:
+                    System.out.println("Enter name of class");
+                    String classname = scanner1.next();
+                    Class classs = new Class(0,classname);
+                    Dbhandler.add_class(connection,classs);
+                    break;
+
+                case 3:
+                    System.out.println("Enter username");
+                    username = scanner1.next();
+                    System.out.println("Enter password");
+                    password = scanner1.next();
+                    System.out.println("Enter classname");
+                    classname = scanner1.next();
+                    user = new user(0,username,password);
+                    classs = new Class(0,classname);
+                    Dbhandler.add_attendance(connection,user,classs);
+
+                    break;
+
+                case 4:
+                    for (user u : user_list ) {
+                        System.out.println(u.getId()+"name- " + u.getUsername());
+                    }
+                    break;
+
+                case 5:
+                    for (Class c : class_list ) {
+                        System.out.println(c.getId()+"name- " + c.getClassname());
+                    }
+                    break;
+
+                case 6:
+                    end = true;
+                    break;
+
+                default:
+                    System.out.println("Choose correct option!!1");
+                    break;
+            }
+        }
     }
 }
